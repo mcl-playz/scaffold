c045dda50f554308a713e7f8d6f0e9576406f91f
Jasper Green <99476285+mcl-playz@users.noreply.github.com>
2026-03-24 11:57:20 +0800

    refactor: make arg resolution internal, add auto-discovering getTarget()

--- src/main/java/org/jasperdev/mcommandframework/models/MCommandContext.java
+++ src/main/java/org/jasperdev/mcommandframework/models/MCommandContext.java
@@ -6,31 +6,13 @@ import org.bukkit.entity.Player;
 
 import javax.annotation.Nonnull;
 import java.util.Map;
-import java.util.NoSuchElementException;
-import java.util.Optional;
 
 public record MCommandContext(
 		@Nonnull CommandSender sender,
 		@Nonnull Command command,
 		@Nonnull String label,
-		@Nonnull Map<String, Object> args // Map name -> value
+		@Nonnull Map<String, Object> args
 ) {
-	@Nonnull
-	public <T> T getArg(String name, Class<T> clazz) {
-		// Use the optional logic internally, but throw if empty
-		return getOptionalArg(name.toLowerCase(), clazz)
-				.orElseThrow(() -> new NoSuchElementException("Argument '" + name + "' missing or wrong type"));
-	}
-
-	@Nonnull
-	public <T> Optional<T> getOptionalArg(String name, Class<T> clazz) {
-		Object val = args.get(name.toLowerCase());
-		if (clazz.isInstance(val)) {
-			return Optional.of(clazz.cast(val));
-		}
-		return Optional.empty();
-	}
-
 	/**
 	 * Gets the first {@link Player} argument, or falls back to the command sender.
 	 *
